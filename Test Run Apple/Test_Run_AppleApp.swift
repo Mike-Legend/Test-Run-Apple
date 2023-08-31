e48a5eb568089bcd2e3f13453a2638ac1e90534b
michael <MGEORGE1@student.fullsail.edu>
2023-08-31 01:14:36 -0700

    Initial Commit

---
+++ Test Run Apple/Test_Run_AppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_Run_AppleApp.swift
+//  Test Run Apple
+//
+//  Created by Michael George on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_Run_AppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
